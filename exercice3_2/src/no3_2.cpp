66b5a039bdb1c67b1898ef4f9cf1cb1566391e39
Guy Bois <guy.bois@polymtl.ca>
2025-02-02 09:58:08 -0500

    Add files via upload

---
+++ exercice3_2/src/no3_2.cpp
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+int glob=100;
+int main()
+{
+  if(fork()==0)
+    { glob=glob-1;
+      printf("ici fils pid=%d, glob=%d, @glob=%p\n", getpid(), glob, &glob); 
+      _exit(0);
+    }
+  int x; wait(&x);
+  glob=glob+1;
+  printf("ici pere pid=%d, glob=%d, @glob=%p\n", getpid(), glob, &glob);
+  return 0;
+}
