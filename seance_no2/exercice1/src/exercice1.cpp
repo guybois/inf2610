718102cf1d78191d0910564769b1ab43e91e0606
Guy Bois <guy.bois@polymtl.ca>
2025-02-02 10:25:05 -0500

    Update exercice1.cpp

--- seance_no2/exercice1/src/exercice1.cpp
+++ seance_no2/exercice1/src/exercice1.cpp
@@ -12,9 +12,9 @@ int main ()
   if ((x = fork()) == 0)	// fils
     {
       printf ("pid=%d, ppid=%d\n", getpid(), getppid());
-      _exit (0);
+      _exit (0);  // Le fils passe de running à zombie
     }
   printf ("pid=%d, pid du fils = %d \n", getpid(), x);
-  wait(NULL);
+  wait(NULL);  // Le fils passe de zombie à terminé
   return 0;
 } 
