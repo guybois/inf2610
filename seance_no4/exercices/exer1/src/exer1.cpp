aaf242001e2920bb91e1379c6d2c5fe2158c1824
Guy Bois <guy.bois@polymtl.ca>
2025-02-06 16:42:58 -0500

    Add files via upload

--- seance_no4/exercices/exer1/src/exer1.cpp
+++ seance_no4/exercices/exer1/src/exer1.cpp
@@ -1,3 +1,23 @@
-
-
-// A venir
+#include <stdio.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+int main(int argc, char* argv[]) {
+    if(argc!=2) _exit(1);
+    int fd[2];
+    char buf;
+    pipe(fd);
+    if (fork() == 0) { // le fils est un lecteur
+        close(fd[1]);
+        while (read(fd[0], &buf, 1) > 0) { write(1, &buf, 1); }
+        write(1, "\n", 1);
+    close(fd[0]);
+    } else { // le père est un écrivain
+        close(fd[0]);
+        write(fd[1], argv[1], strlen(argv[1])+1);
+        close(fd[1]);
+        wait(NULL); // attend la fin de son fils
+    }
+_exit(0);
+}
