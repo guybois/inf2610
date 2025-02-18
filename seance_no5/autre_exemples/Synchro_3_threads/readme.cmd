2b2ae543d8099a722967bba3e26a9a2a42a91801
Guy Bois <guy.bois@polymtl.ca>
2025-02-18 17:19:20 -0500

    Create readme.cmd

---
+++ seance_no5/autre_exemples/Synchro_3_threads/readme.cmd
@@ -0,0 +1,9 @@
+Synchronisation entre 3 threads 
+
+On suppose un procédé  qui demande de synchroniser les 6 fonctions suivantes funcU(), funcV(), funcW(), funcX(), funcY(), funcZ() 
+devant toujours être exécutées dans ce même ordre (U, V, W, X, Y, Z et ainsi de suite) et 3 thread T1(), T2() et T3(). 
+Le thread T1() appelle les fonctions funcU() et funcX(), le thread T2() les fonctions funcV() et funcY() et le thread T3() 
+les fonction funcW() et funcZ(). Écrivez le code des thread T1(), T2() et T3() en veillant à ce que l’ordre d’appel des 
+fonction (U, V, W, X, Y, Z) soit respecté.
+
+Notez que les exercices 4 et 5 sont similaires à celui-ci.  
