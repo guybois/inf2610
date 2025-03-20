580752245d08b5badedf3909d2937791c6d932da
Guy Bois <guy.bois@polymtl.ca>
2025-03-20 15:27:01 -0400

    Create readmed.cmd

---
+++ seance_no7/rdv/readmed.cmd
@@ -0,0 +1,4 @@
+Ce numéro est une façon différente de faire l'implémentation de de l'exercice 6 du chapitre 5.
+Un groupe de 3 amis se donnent rendez-vous au cinéma. Ils doivent s'attendre à l'entrée du cinéma. 
+Ensuite, ils entrent ensemble et assistent à la projection d’un film. Le comportement de chaque ami
+est simulé par le thread ami. Proposez un code avec variable de condition et mutex POSIX pour effectuer ce rendez-vous.
